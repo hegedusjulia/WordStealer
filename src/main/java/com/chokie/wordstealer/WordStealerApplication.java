090660c64ddefe0a2c3f48eafb20d590ceac4a41
hegedusjulia <hegedus.julia.erzsebet@gmail.com>
2022-04-27 00:18:55 +0200

    Initial commit

---
+++ src/main/java/com/chokie/wordstealer/WordStealerApplication.java
@@ -0,0 +1,13 @@
+package com.chokie.wordstealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordStealerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordStealerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chokie/wordstealer/WordStealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chokie.wordstealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordStealerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
